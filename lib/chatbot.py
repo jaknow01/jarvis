f845e6f91a577024155bd8f8cf5b7ca2439ff5ee
Kuba <jakub.nowakowski01@onet.pl>
2025-10-24 17:11:26 +0200

    is aware of previous messages

--- lib/chatbot.py
+++ lib/chatbot.py
@@ -1,34 +1,32 @@
 from lib.agents import create_coordinator_agent
-from agents import OpenAIResponsesModel, AsyncOpenAI, RunConfig, Runner
-import os
+from agents import Runner
+from lib.cache import Cache
+from lib.run_config import Config
 
 class Chatbot():
     async def start_chatbot(self):
         print("Wiadomość powitalna")
+        cache = Cache()
 
         while(True):
             text = input()
-            
-            client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-            model = OpenAIResponsesModel(
-                model=os.getenv("OPENAI_DEFAULT_MODEL"),
-                openai_client=client
-            )
-
-            run_config = RunConfig(model=model, model_provider=client)
+            run_config = Config.create_config()
             coordinator = create_coordinator_agent()
 
+            prev_id = await cache.get_from_cache("previous_response_id")
             result = await Runner.run(
                 coordinator,
                 input = text,
                 run_config=run_config,
+                previous_response_id= prev_id
             )
 
+            await cache.save_to_cache("previous_response_id", result.last_response_id)
             print(result.final_output)
 
 # TODO
-# 1. Zacząć zachowywać previous response id w redisie -> czyli napisać kod do zapisu i odczytu z redisa
+# 1. Zacząć zachowywać previous response id w redisie -> czyli napisać kod do zapisu i odczytu z redisa --DONE
 # 2. Ogarnąć co z tym ctx dla narzędzi - potencjalnie kod dla tego kontekstu ale nie wiem co by się miało w nim znajdować
-# 3. Przenieść tworzenie run_configu do oddzielnego pliku
+# 3. Przenieść tworzenie run_configu do oddzielnego pliku --DONE
 # 4. Jakieś testowe narzędzia
 # 5. ZAPOMNIEĆ O LANGCHAINIE - ALL MY HOMIES HATE LANGCHAIN
