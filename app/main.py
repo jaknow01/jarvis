418d7929d5d7f74f1007980b5c11a7e5856c61b6
Kuba <jakub.nowakowski01@onet.pl>
2025-11-04 21:30:17 +0100

    working logger

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from lib.chatbot import Chatbot
 from dotenv import load_dotenv
 import asyncio
+from lib.logger import Logger
+import logging
+
+logger = logging.getLogger(__name__)
 
 def main():
     load_dotenv()
+    Logger.config_root_logger()
+    logger.info("Initializing chatbot")
+
     chatbot = Chatbot()
+    
     asyncio.run(chatbot.start_chatbot())
 
 if __name__ == "__main__":
